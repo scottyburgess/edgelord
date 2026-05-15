ab6be450e608c55e696bfe4e15bded6a2cfc2aa6
Scott Burgess <scoburgess@gmail.com>
2026-05-15 15:03:10 -0700

    Phase 0: UE5 Third Person C++ template

---
+++ Source/Edgelord/Edgelord.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Edgelord : ModuleRules
+{
+	public Edgelord(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Edgelord",
+			"Edgelord/Variant_Platforming",
+			"Edgelord/Variant_Platforming/Animation",
+			"Edgelord/Variant_Combat",
+			"Edgelord/Variant_Combat/AI",
+			"Edgelord/Variant_Combat/Animation",
+			"Edgelord/Variant_Combat/Gameplay",
+			"Edgelord/Variant_Combat/Interfaces",
+			"Edgelord/Variant_Combat/UI",
+			"Edgelord/Variant_SideScrolling",
+			"Edgelord/Variant_SideScrolling/AI",
+			"Edgelord/Variant_SideScrolling/Gameplay",
+			"Edgelord/Variant_SideScrolling/Interfaces",
+			"Edgelord/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/Edgelord/Edgelord.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Edgelord.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Edgelord, "Edgelord" );
+
+DEFINE_LOG_CATEGORY(LogEdgelord)
